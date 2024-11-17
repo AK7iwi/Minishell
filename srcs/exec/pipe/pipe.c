074b9f34f036bfbbfe7745032943e149595203b1
Dilara Guler <dilaraguler@MacBook-Air-de-Dilara.local>
2024-11-17 15:56:52 +0100

    pipe OK, a verifier avec ()

--- srcs/exec/pipe/pipe.c
+++ srcs/exec/pipe/pipe.c
@@ -57,66 +57,93 @@
 // 	close(tube[1]);
 // 	return (WIFEXITED(status) && !WEXITSTATUS(status));
 // }
-bool	handle_pipe(t_data *data)
+static bool create_pipe(int tube[2], t_data *data)
+{
+	if (pipe(tube) == -1)
+	{
+		data->error.exec_errors |= ERROR_PIPE;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static bool fork_and_exec_left_cmd(t_data *data, t_ast *current_ast, int tube[2], int prev_fd)
+{
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		if (prev_fd != -1)
+		{
+			dup2(prev_fd, STDIN_FILENO);
+			close(prev_fd);
+		}
+		dup2(tube[1], STDOUT_FILENO);
+		close(tube[0]);
+		close(tube[1]);
+		exec(data, current_ast->operator.left);
+		free_all(data);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == -1)
+	{
+		data->error.exec_errors |= ERROR_FORK;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static bool exec_final_command(t_data *data, t_ast *current_ast, int prev_fd)
+{
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+		exec(data, current_ast);
+		free_all(data);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == -1)
+	{
+		data->error.exec_errors |= ERROR_FORK;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static void close_unused_fds(int prev_fd, int tube[2])
+{
+	close(tube[1]);
+	if (prev_fd != -1)
+		close(prev_fd);
+}
+
+bool handle_pipe(t_data *data)
 {
 	int tube[2];
 	int prev_fd = -1;
 	t_ast *current_ast = data->ast;
-	pid_t pid;
 	int status;
 
 	while (current_ast && current_ast->type == AST_OPERATOR && current_ast->operator.type == OP_PIPE)
 	{
-		if (pipe(tube) == -1) // Crée un nouveau tube pour chaque étape de la chaîne
-			return (data->error.exec_errors |= ERROR_PIPE, EXIT_FAILURE);
-		
-		// Crée un processus pour la commande de gauche
-		pid = fork();
-		if (pid == 0)
-		{
-			if (prev_fd != -1) // Utilise l'entrée du tube précédent, s'il existe
-			{
-				dup2(prev_fd, STDIN_FILENO);
-				close(prev_fd);
-			}
-			dup2(tube[1], STDOUT_FILENO); // Sortie vers le tube courant
-			close(tube[0]);
-			close(tube[1]);
-			exec(data, current_ast->operator.left);
-			free_all(data);
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == -1)
-			return (data->error.exec_errors |= ERROR_FORK, false);
-		
-		// Ferme les descripteurs de tubes inutilisés dans le processus parent
-		close(tube[1]);
-		if (prev_fd != -1)
-			close(prev_fd);
-		
-		// Prépare le tube courant pour le prochain tour
+		if (create_pipe(tube, data) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		if (fork_and_exec_left_cmd(data, current_ast, tube, prev_fd) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		close_unused_fds(prev_fd, tube);
 		prev_fd = tube[0];
-		current_ast = current_ast->operator.right; // Passe à la commande suivante
+		current_ast = current_ast->operator.right;
 	}
-
-	// Exécute la dernière commande sans créer un autre pipe
 	if (current_ast && prev_fd != -1)
 	{
-		pid = fork();
-		if (pid == 0)
-		{
-			dup2(prev_fd, STDIN_FILENO);
-			close(prev_fd);
-			exec(data, current_ast);
-			free_all(data);
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == -1)
-			return (data->error.exec_errors |= ERROR_FORK, false);
+		if (exec_final_command(data, current_ast, prev_fd) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
 		close(prev_fd);
 	}
-
-	// Attends tous les processus enfants
 	while (wait(&status) > 0);
 	return (WIFEXITED(status) && !WEXITSTATUS(status));
 }
+
